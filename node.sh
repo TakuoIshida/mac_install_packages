e3760d14f631d65610569ecf1bf492215a198618
Tishida <archersparadox.100@gmail.com>
2021-10-23 11:33:32 +0900

    init

---
+++ node.sh
@@ -0,0 +1,5 @@
+brew install nodebrew
+nodebrew -v
+nodebrew setup
+# exec set_node_version.sh
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
---
+++ set_node_version.sh
@@ -0,0 +1,5 @@
+nodebrew ls
+#ex.. nodebrew use v16.12.0 
+nodebrew ls
+source ~/.bash_profile
+node -v
