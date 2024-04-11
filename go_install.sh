1cf272ebad7dce879d523f5389c241298c8308bc
Tishida <archersparadox.100@gmail.com>
2024-04-11 13:56:45 +0900

    debugerの追加

--- go_install.sh
+++ go_install.sh
@@ -1,8 +1,14 @@
 #!bin/bash
+brew update
 brew install go
 # ファイルでdownloadもできる
 # https://go.dev/doc/install
 
 go version
+
+# debugerのinstall
+brew install delve
+dlv version
+
 # installしたpackageのpathを設定しないとpakcageを実行できない
 export PATH=$PATH:$HOME/go/bin
